a26e1adb0540986b3cb3111df4e57cfb46c43866
fuupii <fuuma.t1110@icloud.com>
2023-02-09 12:04:29 +0900

    edit update

--- resources/views/owner/images/index.blade.php
+++ resources/views/owner/images/index.blade.php
@@ -14,13 +14,13 @@
                 </div>
                 <div class="flex flex-wrap">
                     @foreach ($images as $image)
-                        <div class="w-1/4 p-4">
+                        <div class="w-1/4 p-2 md:p-4">
                         <a href="{{ route('owner.images.edit', ['image' => $image->id ]) }}">
-                            <div class="border randed-md p4">
-                                <div class="text-xl">
-                                    {{ $image->title }}
-                                </div>
+                            <div class="border randed-md p-2 md:p4">
                                 <x-thumbnail :filename="$image->filename" type="products" />
+                                    <div class="text-gray-700">
+                                        {{ $image->title }}
+                                    </div>
                             </div>
                         </a>
                         </div>
