bce4f11eca7570fccb6c1517b48c90200e99e8ff
fuupii <fuuma.t1110@icloud.com>
2023-02-17 19:46:50 +0900

    productCreate

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import './bootstrap';
 
+console.log('ijij');
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -28,7 +28,7 @@
                                         </select>
                                     </div>
                                 </div>
-
+                                <x-select-image name="image1" />
 
                             <div class="flex mt-4 p-2 w-full justify-around">
                                 <button type="button" onclick="location.href='{{ route('owner.products.index') }}'" class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
