a058836e920edce02c784936661fa5d6010cbebb
fuupii <fuuma.t1110@icloud.com>
2023-03-28 16:11:18 +0900

    product create

--- database/migrations/2023_02_09_151857_create_products_table.php
+++ database/migrations/2023_02_09_151857_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,11 +16,19 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('information');
+            $table->unsignedInteger('price');
+            $table->boolean('is_selling');
+            $table->integer('sord_order')->nullable();
             $table->foreignId('shop_id')->constrained()
             ->onUpdate('cascade')
             ->onDelete('cascade');
             $table->foreignId('secondary_category_id')->constrained();
             $table->foreignId('image1')->nullable()->constrained('images');
+            $table->foreignId('image2')->nullable()->constrained('images');
+            $table->foreignId('image3')->nullable()->constrained('images');
+            $table->foreignId('image4')->nullable()->constrained('images');
             $table->timestamps();
         });
     }
