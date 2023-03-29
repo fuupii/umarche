1e93e3cc7a8b4945fbd4b2cc8fcb0e6ce1d7509d
fuupii <fuuma.t1110@icloud.com>
2023-03-29 16:49:40 +0900

    product store

--- resources/views/owner/products/index.blade.php
+++ resources/views/owner/products/index.blade.php
@@ -18,7 +18,7 @@
                         <div class="w-1/4 p-2 md:p-4">
                         <a href="{{ route('owner.products.edit', ['product' => $product->id ]) }}">
                             <div class="border randed-md p-2 md:p4">
-                                <x-thumbnail :filename="$product->imageFirst->filename" type="products" />
+                                <x-thumbnail filename="{{$product->imageFirst->filename ?? ''}}" type="products" />
                                     <div class="text-gray-700">
                                         {{-- {{ $product->name }} --}}
                                     </div>
